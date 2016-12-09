0a5bca2b482abb354fb9b341a9ab5d7bff201bfd
wwang126 <wavelet.wang@gmail.com>
2016-12-09 03:09:48 -0800

    added 404 page and basic static server routing. Readded alpha saving feature but looking into alternate method.

--- public/index.js
+++ public/index.js
@@ -277,3 +277,10 @@ function setColor(rd, gr, bl){
 canvas.addEventListener('mousedown', handleMouseButtonDown);
 canvas.addEventListener('mousemove', handleMouseButtonMove);
 document.addEventListener('mouseup', handleMouseButtonUp);
+
+var saveCanvas = document.getElementById("save");
+function save(){
+  var canvasData = canvas.toDataURL("image/png");
+  this.href = canvasData;
+}
+saveCanvas.addEventListener('click',save,false);
